ca7b6bd5c3a0186985fa721350c4c372179260d9
Diego Moronha <diegomoronha@gmail.com>
2025-08-22 00:47:14 -0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { configDefaults } from 'vitest/config'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    envPrefix: 'FE_',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    test: {
+      include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      globals: true,
+      coverage: {
+        providers: ['v8'],
+        reporters: ['text', 'html', 'lcov', 'json'],
+        exclude: [...configDefaults.exclude, 'public/*'],
+      },
+    },
+  }
+})
